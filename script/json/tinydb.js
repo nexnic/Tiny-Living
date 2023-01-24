c6dd25c545960a4f7fe20d5cf53ddfe31d7816e0
Kent Erik Hole <kent.erik.hole@outlook.com>
2023-01-24 04:48:43 +0100

    adding templet fil

--- script/json/tinydb.js
+++ script/json/tinydb.js
@@ -4,5 +4,60 @@
     'use strict';
 
 let tl = {
-    
-}
+    [0]: {
+        // Title of the product 
+        // Name of product
+        name: "name",
+
+        // This will be the short description on product
+        short_desciption: "",
+
+        // This will be longer Description for this Product
+        description: "",
+
+        // This will stat if Privat rent or Company 
+        rentagent: {
+            privat: true,
+            company: false,
+        },
+
+        // This will be the price for the file
+        price: 200, 
+        
+        // This will be the Deposit for the product 
+        Deposit: 1000, 
+        
+        // This wil be the specific for the site
+        // Like bedroom, area, rent Period, floor, facilities, 
+        spec: {
+            bedroom: 1, 
+            area: "20m2",
+            rentalperiod: "data",
+            floor: 1,
+            facilities: "",
+        },
+        
+        // We need address field for the product 
+        // country, City, Adreess
+        
+        address: {
+            contry: "",
+            city: "",
+            address: "",
+        },
+
+        // This site have 2 type of category
+        // Van, Tinyhouse
+
+        category: {
+            tinyhouse: true,
+            vans: false, 
+        },
+        img: {
+            [0]: "URL",
+            [1]: "URL",
+            [2]: "URL",
+        },
+
+    },
+};
