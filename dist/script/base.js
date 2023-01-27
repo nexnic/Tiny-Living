064c59b81b2405b94659a2ead9913482b60b8320
Kent Erik Hole <kent.erik.hole@outlook.com>
2023-01-27 20:25:52 +0100

    adding the function for modal

--- dist/script/base.js
+++ dist/script/base.js
@@ -4,8 +4,13 @@
 // Dom Tree 
 
     // Profile Site 
+        // Head of modal 
+            // Modal Header Profile 
+            const mhp = document.querySelector('#modal-Profile-title')
         // Body of modal
-            const mbp =  document.querySelector('#modal-Profile-body')
+            // Modal Body Profile
+            const mbp =  document.querySelector('#modal-Profile-body');
+        
 
 
 
@@ -18,12 +23,34 @@
 
 
 
-// Function Modal for setting on profile 
-    function 
+// Function Modal for setting on profile
+    // value1 
+    function modelprofile(value1) {
+        console.log(value1)
 
-/*
-    <div class="mb-3">
-                <label for="recipient-name" class="col-form-label"></label>
-                <input type="file" name="" id="upload-email" class="form-control">
-    </div>
-*/
+        // Profile Title Image
+            const pti = 'Change image'
+        // Profile Body Image
+            const pbi = 'Press save ofter adding image'
+
+        if(value1 === "image") {
+            // Use for test
+            // console.log('test ok')
+
+            // Headline 
+            mhp.innerHTML += `
+                ${pti}
+
+            `;
+            mbp.innerHTML += `
+                <div class="mb-3">
+                    <label for="recipient-name" class="col-form-label"></label>
+                    <input type="file" name="" id="upload-email" class="form-control">
+                    <p>${pbi}</p>
+                </div>
+            `
+            
+
+        }
+    }
+
