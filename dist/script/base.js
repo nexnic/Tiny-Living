13d5f30d74f93381ba2b734f037495f67327adde
Kent Erik Hole <kent.erik.hole@outlook.com>
2023-01-27 19:59:17 +0100

    start script on the modal function

---
+++ dist/script/base.js
@@ -0,0 +1,29 @@
+// Using Strict mode on 
+    'use strict mode';
+
+// Dom Tree 
+
+    // Profile Site 
+        // Body of modal
+            const mbp =  document.querySelector('#modal-Profile-body')
+
+
+
+
+
+
+
+
+
+
+
+
+// Function Modal for setting on profile 
+    function 
+
+/*
+    <div class="mb-3">
+                <label for="recipient-name" class="col-form-label"></label>
+                <input type="file" name="" id="upload-email" class="form-control">
+    </div>
+*/
