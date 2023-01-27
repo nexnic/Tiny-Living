04e6c7b0dca0af306bb26d6f5d37b6808aaed6a1
Kent Erik Hole <kent.erik.hole@outlook.com>
2023-01-27 19:47:06 +0100

    Start on Templet for the feed

---
+++ dist/script/data.js
@@ -0,0 +1,19 @@
+// Strict mode on 
+    'use strict'
+
+// The array for Feed
+
+let tlFeed = [
+    {
+        title: 'The ultimate freedom',
+        short_desciption: "This place where you will get peach of mind. ",
+        image: {
+            [0]: ""
+        },
+        meta: {
+            userName: "",
+            UserImage: "", 
+        }
+
+    }
+]
